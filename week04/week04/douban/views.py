a29b6cba483499d722a6c6171b1040e09b8b8d6f
hjjof001 <hjjof0001@163.com>
2020-12-21 00:34:51 +0800

    add search

--- week04/week04/douban/views.py
+++ week04/week04/douban/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
 from .models import Movie, Comment
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def movie_comment(request, mid):
     """
     返回指定电影的评论、星级、日期
@@ -15,7 +17,13 @@ def movie_comment(request, mid):
     movie_name = query_set[0].name
 
     # 评论
-    comment = Comment.objects.filter(mid=mid)
+    if request.method == "GET":
+        comment = Comment.objects.filter(mid=mid)
+    elif request.method == "POST":
+        q = request.POST.get('q')
+        comment = Comment.objects.filter(mid=mid).filter(comment__contains=q)
+    
+    #comment = Comment.objects.filter(mid=mid)
     for item in comment:
         item.date = item.date.strftime('%Y-%m-%d')
 
